039e0b46a5e06219b6a9f6911e902cf86d49efb2
cutiko <cutiko@gmail.com>
2016-07-29 15:41:36 -0400

    ADD: phone call permissions and UI. WARNING: target downed

--- app/src/main/java/cl/cutiko/aboutcutiko/ContactActivity.java
+++ app/src/main/java/cl/cutiko/aboutcutiko/ContactActivity.java
@@ -1,7 +1,11 @@
 package cl.cutiko.aboutcutiko;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        final TextView phoneTv = (TextView) findViewById(R.id.phone);
+        phoneTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:"+phoneTv.getText().toString()));
+                startActivity(callIntent);
+            }
+        });
     }
 }
