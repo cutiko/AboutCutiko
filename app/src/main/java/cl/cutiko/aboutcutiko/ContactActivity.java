533276619e79e9a9fd861f7bc2d20b4298317d49
cutiko <cutiko@gmail.com>
2016-07-29 16:38:11 -0400

    ADD: basic intents for contact. WIP: send email

--- app/src/main/java/cl/cutiko/aboutcutiko/ContactActivity.java
+++ app/src/main/java/cl/cutiko/aboutcutiko/ContactActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
@@ -21,6 +22,30 @@ public class ContactActivity extends AppCompatActivity {
                 Intent callIntent = new Intent(Intent.ACTION_CALL);
                 callIntent.setData(Uri.parse("tel:"+phoneTv.getText().toString()));
                 startActivity(callIntent);
+            }
+        });
+
+        TextView linkedin = (TextView) findViewById(R.id.linkedin);
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent linkedinIntent = new Intent(Intent.ACTION_VIEW);
+                linkedinIntent.setData(Uri.parse("https://cl.linkedin.com/in/cutiko"));
+                startActivity(linkedinIntent);
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.contactBtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setType("*/*");
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"cutiko@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "");
+                intent.putExtra(Intent.EXTRA_TEXT, "body");
+                startActivity(intent);
             }
         });
     }
